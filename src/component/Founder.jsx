ad07cae7ac0292b6dd6a4c9d87e4cc34e51e9841
Mohamed200400 <mohamedamine200400@gmail.com>
2024-07-22 10:13:00 +0200

    gg

--- src/component/Founder.jsx
+++ src/component/Founder.jsx
@@ -29,14 +29,7 @@ const Founder = () => {
         <div className='w-100 imgc'><img src={img} alt='xt' className='img '/></div>
         <h1 className='text-center mb-2 aa colw'>مقياس تقدير الذات للكبار</h1>
         <h5 className='text-center mb-4 colw'>Self-Esteem Scale for adults </h5>
-        <p className=' text mt-5 mb-5  colw'>يهدف المقياس إلى الكشف
-عن مستويات تقدير الذات
-لدى الكبار من خالل فقرات
-وعبارات تتلاءم مع قيم
-وثقافة المجتمع، ويحتوي
-على أبعاد متعددة تكشف عن
-العوامل التي يتشكل منها
-تقديرنا ألنفسنا. </p>
+        <p className=' text mt-5 mb-5  colw'>يهدف المقياس إلى الكشف عن مستويات تقدير الذات لدى الكبار، وبعد الانتهاء من الإجابة على الفقرات يصدر تقرير يوضح مستوى تقديرك لذاتك</p>
         <p className='text-center text colw'>إعداد</p>
         <p className='text-center mb-2 text colw'>الدكتور  فيصل بن ناصر الشدوخي </p>
         <div className='w-100 tt1'><button className=' mt-5 mb-3 start '><Link className=' text start' to={'/login'}>إبدأ الان <FaArrowLeft/></Link></button></div>
